a5cfa8eed884bc0798b2fb65d577ffbb7edabe35
Shiv <sky.shivy@gmail.com>
2021-06-20 17:10:17 +0530

    filenew pod file adde

---
+++ Gamification.xcframework/ios-arm64_x86_64-simulator/Gamification.framework/Headers/Gamification.h
@@ -0,0 +1,18 @@
+//
+//  Gamification.h
+//  Gamification
+//
+//  Created by SKY on 27/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gamification.
+FOUNDATION_EXPORT double GamificationVersionNumber;
+
+//! Project version string for Gamification.
+FOUNDATION_EXPORT const unsigned char GamificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gamification/PublicHeader.h>
+
+
