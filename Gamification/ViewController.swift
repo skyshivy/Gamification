42891110d01ecd8182afbadc5a97fa2809821328
Shiv <sky.shivy@gmail.com>
2021-06-20 17:10:58 +0530

    time and date added

--- Gamification/ViewController.swift
+++ Gamification/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //20 june 5:10
         // Do any additional setup after loading the view.
     }
 
